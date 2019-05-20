f039aa4b77513d6f0b4a8a336ee7cbe5b9d045dd
shizhijie <2272684702@qq.com>
2019-05-20 12:55:52 +0800

    for update

--- microservicecloud-provider-dept-8001/src/main/java/com/zjs/bwcx/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/zjs/bwcx/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.zjs.bwcx.springcloud.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,23 +16,41 @@ import com.zjs.bwcx.springcloud.service.DeptService;
 
 @RestController
 public class DeptController {
-	
+
 	@Autowired
-    private DeptService service;
-	
+	private DeptService service;
+
 	@RequestMapping(value = "/dept/add", method = RequestMethod.POST)
-    public boolean add(@RequestBody Dept dept)
-    {
-        return service.addDept(dept);
-    }
+	public boolean add(@RequestBody Dept dept) {
+		return service.addDept(dept);
+	}
 
-    @RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
-    public Dept get(@PathVariable("id") Long id){
-        return service.findById(id);
-    }
+	@RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
+	public Dept get(@PathVariable("id") Long id) {
+		return service.findById(id);
+	}
 
-    @RequestMapping(value = "/dept/list", method = RequestMethod.GET)
-    public List<Dept> list(){
-        return service.findAll();
-    }
+	@RequestMapping(value = "/dept/list", method = RequestMethod.GET)
+	public List<Dept> list() {
+		return service.findAll();
+	}
+
+	@Autowired
+	private DiscoveryClient client;
+
+	@RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+	public Object discovery() {
+		List<String> list = client.getServices();
+		System.out.println("**********" + list);
+		// 得到所有的service
+
+		List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
+		// 看是否有名字和MICROSERVICECLOUD-DEPT一样的接口
+		for (ServiceInstance element : srvList) {
+			System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+					+ element.getUri());
+		}
+		return this.client;
+	}
+
 }
